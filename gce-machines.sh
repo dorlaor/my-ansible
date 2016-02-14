774269baf7d71a54b5a8319a559f760b8da93fd3
Dor Laor <dor@cloudius-systems.com>
2016-02-14 23:47:03 +0200

    Ease the job of creating servers and clients

--- gce-machines.sh
+++ gce-machines.sh
@@ -1,6 +1,40 @@
+#!/bin/sh -e
+#
+#  Copyright (C) 2016 ScyllaDB
 
-# Very large machine
-gcloud compute instances create $NAME \
+print_usage() {
+    echo "gce-machines -c [clients] -s [servers]"
+    echo "  -c  Amount of clients"
+    echo "  -s  Amount of servers"
+    exit 1
+}
+
+CLIENTS=0
+SERVERS=0
+while getopts s:c:h OPT; do
+    case "$OPT" in
+        "s")
+            SERVERS=$OPTARG
+            ;;
+        "c")
+            CLIENTS=$OPTARG
+            ;;
+        "h")
+            print_usage
+            ;;
+    esac
+done
+
+wait_n() {
+    for ((i=0 ; i < $1 ; i++ )); do
+        wait
+    done
+}
+
+echo "About to create $SERVERS servers"
+
+for ((i=0 ; i < $SERVERS ; i++ )); do
+gcloud compute instances create server-$i \
     --machine-type n1-standard-32 \
     --local-ssd interface=nvme \
     --local-ssd interface=nvme \
@@ -11,13 +45,23 @@ gcloud compute instances create $NAME \
     --local-ssd interface=nvme \
     --local-ssd interface=nvme \
     --image centos-7-v20160126 \
-    --image-project centos-cloud
+    --image-project centos-cloud &
+done
 
-# Client machine
-gcloud compute instances create $NAME \
+wait_n $SERVERS
+
+echo "About to create $CLIENTS clients"
+
+for ((i=0 ; i < $CLIENTS ; i++ )); do
+gcloud compute instances create client-$i \
     --machine-type n1-standard-16 \
     --local-ssd interface=nvme \
     --local-ssd interface=nvme \
     --image centos-7-v20160126 \
-    --image-project centos-cloud
+    --image-project centos-cloud &
+done
+
+wait_n $CLIENTS
+
+gcloud compute instances list
 
