ee91cc1db12e2b7c40a247400028eec404884f89
Dor Laor <dor@cloudius-systems.com>
2016-02-19 00:46:28 +0200

    Allow configurable server group type/name

--- step_3_gce_configure_machines.sh
+++ step_3_gce_configure_machines.sh
@@ -1,3 +1,29 @@
-ansible -i ./hosts.init servers -m copy -a "src=/home/dor/src/my-ansible/scylla-setup-script.sh dest=/home/dor/ mode=0777"  -vv
-ansible -i ./hosts.init servers -m shell -a "sudo /home/dor/scylla-setup-script.sh" -vv
+#!/bin/sh -e
+#
+#  Copyright (C) 2016 ScyllaDB
+
+print_usage() {
+    echo "gce-machines [-t TYPE] -a"
+    echo "  -a  All machines"
+    echo "  -t  just the TYPE machines"
+    exit 1
+}
+
+STR=0
+while getopts :at:h OPT; do
+    case "$OPT" in
+        "a")
+            STR="all"
+            ;;
+        "t")
+            STR=$OPTARG
+            ;;
+        "h")
+            print_usage
+            ;;
+    esac
+done
+
+ansible -i ./hosts.init $STR -m copy -a "src=/home/dor/src/my-ansible/scylla-setup-script.sh dest=/home/dor/ mode=0777"  -vv
+ansible -i ./hosts.init $STR -m shell -a "sudo /home/dor/scylla-setup-script.sh" -vv
 
