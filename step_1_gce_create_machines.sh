4ec48588b7808488661ddad5b9f1378491920866
Dor Laor <dor@cloudius-systems.com>
2016-02-21 00:54:04 +0200

    add an option to start with a certain index

--- step_1_gce_create_machines.sh
+++ step_1_gce_create_machines.sh
@@ -6,15 +6,20 @@ print_usage() {
     echo "gce-machines -c [clients] -s [servers]"
     echo "  -c  Amount of clients"
     echo "  -s  Amount of servers"
+    echo "  -i  Start counting from i=x [default=0]"
     exit 1
 }
 
 CLIENTS=0
 SERVERS=0
-while getopts s:c:h OPT; do
+INDX=0
+while getopts c:si:h OPT; do
     case "$OPT" in
         "s")
             SERVERS=$OPTARG
+            ;;
+        "i")
+            INDX=$OPTARG
             ;;
         "c")
             CLIENTS=$OPTARG
@@ -26,14 +31,14 @@ while getopts s:c:h OPT; do
 done
 
 wait_n() {
-    for ((i=0 ; i < $1 ; i++ )); do
+    for ((i=$INDX ; i < $1 ; i++ )); do
         wait
     done
 }
 
 echo "About to create $SERVERS servers"
 
-for ((i=0 ; i < $SERVERS ; i++ )); do
+for ((i=$INDX ; i < $SERVERS ; i++ )); do
 gcloud compute instances create server-$i \
     --machine-type n1-standard-32 \
     --local-ssd interface=nvme \
@@ -52,7 +57,7 @@ wait_n $SERVERS
 
 echo "About to create $CLIENTS clients"
 
-for ((i=0 ; i < $CLIENTS ; i++ )); do
+for ((i=$INDX ; i < $CLIENTS ; i++ )); do
 gcloud compute instances create client-$i \
     --machine-type n1-standard-16 \
     --local-ssd interface=nvme \
