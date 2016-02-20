32d42d88fd145c1b3ed0c0d1b364dadfb425f035
Dor Laor <dor@cloudius-systems.com>
2016-02-20 23:34:30 +0200

    Add bunch of utilities for handling VMs

--- step_2_ansible_host_file.sh
+++ step_2_ansible_host_file.sh
@@ -1,14 +1,54 @@
 #!/bin/bash
+#
+#  Copyright (C) 2016 ScyllaDB
+
+print_usage() {
+    echo "NAME [-f] [-i]"
+    echo "  -f Create ansible host file"
+    echo "  -i get servers internal IPs"
+    echo "  -k kill all machines"
+    exit 1
+}
+
+FILE=0
+IP=0
+KILL=0
+while getopts :kfih OPT; do
+    case "$OPT" in
+        "f")
+            FILE=1
+            ;;
+        "i")
+            IP=1
+            ;;
+        "k")
+            KILL=1
+            ;;
+        "h")
+            print_usage
+            ;;
+    esac
+done
+
 
 CLIENTS="n1-standard-16"
 SERVERS="n1-standard-32"
 HOSTS_FILE='hosts.init'
 
-echo "[clients]" >> $HOSTS_FILE
-gcloud compute instances list | grep $CLIENTS | awk '{print $5}' >> $HOSTS_FILE
+if [ $FILE -eq 1 ]; then
+   echo "[clients]" >> $HOSTS_FILE
+   gcloud compute instances list | grep $CLIENTS | awk '{print $5}' >> $HOSTS_FILE
+   echo "" >> $HOSTS_FILE
 
-echo "" >> $HOSTS_FILE
+   echo "[servers]" >> $HOSTS_FILE
+   gcloud compute instances list | grep $SERVERS | awk '{print $5}' >> $HOSTS_FILE
+fi
 
-echo "[servers]" >> $HOSTS_FILE
-gcloud compute instances list | grep $SERVERS | awk '{print $5}' >> $HOSTS_FILE
+if [ $IP -eq 1 ]; then
+   gcloud compute instances list | grep $SERVERS | awk '{print $4}'
+fi
 
+if [ $KILL -eq 1 ]; then
+   MACHINES=$(gcloud compute instances list | grep n1-standard | awk '{print $1}' | xargs)
+   gcloud compute instances delete $MACHINES
+fi
