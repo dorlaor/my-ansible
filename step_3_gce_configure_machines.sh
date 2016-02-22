c7eef9187eddca459be535db05e7794fcd4c2110
Dor Laor <dor@cloudius-systems.com>
2016-02-22 11:00:00 +0200

    automatically gce-ssh into the machines in order to load the keys into them

--- step_3_gce_configure_machines.sh
+++ step_3_gce_configure_machines.sh
@@ -24,6 +24,11 @@ while getopts t:ah OPT; do
     esac
 done
 
+#gce-ssh into one of the machines and exit.
+#this is a must in order to load the ssh keys into the machines
+#otherwise ansible won't be able to ssh them
+echo "exit" | gcloud compute ssh `gcloud compute instances list | awk '{print $1}' | head -2 | tail -1`
+
 ansible -i ./hosts.init $STR -m copy -a "src=/home/dor/src/my-ansible/scylla-setup-script.sh dest=/home/dor/ mode=0777"  -vv
 ansible -i ./hosts.init $STR -m shell -a "sudo /home/dor/scylla-setup-script.sh" -vv
 
